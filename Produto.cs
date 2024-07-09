327a29ad54242c1c12d08a3e00035bffebcc28f2
MURILO ALMEIDA <cmodox@gmail.com>
2024-07-09 14:11:53 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque {
+    internal class Produto {
+
+        public string Nome { get; private set; }//Propriedade serve para os dados serem acessados normalmente no programa principal mas nao alterados
+        public float Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, float preco, int quantidade) {// Construtor para que o objeto ja seja criado com valores
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public float ValorTotalEstoque() {// Faz o calculo do valor em estoque
+            return Preco * Quantidade;
+        }
+        public void AdicinarProdutos(int quantidade) {// Adiciona produtos no estoque
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {// Remove produtos do estoque
+            Quantidade = Quantidade - quantidade;
+        }
+        public override string ToString() {// Faz com que consiga printar as informacoes sem concatenacao
+            return Nome + ", R$" + Preco.ToString(CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total R$ " + ValorTotalEstoque().ToString(CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Estoque {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int qtd;
+            string nome;
+            float preco;
+            int quantidade;
+            int quantProdutos;
+
+            Console.WriteLine("Digite a quantidade de produtos que quer acrecentar: ");
+            quantProdutos = int.Parse(Console.ReadLine());
+
+            Produto[] Produtos = new Produto[quantProdutos];
+
+            for (int i = 0; i < quantProdutos; i++) {
+                //Entrando com os dados do produto
+                Console.WriteLine("Entre com os dados do produto");
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+
+                Console.Write("Preco: ");
+                preco = float.Parse(Console.ReadLine());
+
+                Console.Write("Quantidade: ");
+                quantidade = int.Parse(Console.ReadLine());
+
+                Produto p = new Produto(nome, preco, quantidade);
+                Produtos[i] = p;
+
+                //Adiciona produtos com a funcao
+                Console.WriteLine("Dados do produto: " + p);
+                Console.WriteLine("Digite a quantidade de produtos a ser adicionado ao estoque:");
+                qtd = int.Parse(Console.ReadLine());
+                p.AdicinarProdutos(qtd);
+                Console.WriteLine("Dados atualizados para: " + p);
+
+                //Remove produtos com a funcao
+                Console.WriteLine("Digite a quantidade de produtos a ser removido do estoque:");
+                qtd = int.Parse(Console.ReadLine());
+                p.RemoverProdutos(qtd);
+                Console.WriteLine("Dados atualizados para: " + p);
+            }
+            Console.WriteLine("Resumo do estoque");
+            foreach (Produto produto in Produtos) Console.WriteLine(produto.ToString());
+        }
+
+    }
+}
+        
